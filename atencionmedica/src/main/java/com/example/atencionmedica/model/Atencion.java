c5f83a29d1f20702e7b1cb79146763a391c484d5
agusvalenzuela <agustin.valenzuel@gmail.com>
2024-04-14 16:46:45 -0400

    Configuracion archivos de carpeta model, uso de librería jakarta y su aplicacion

--- atencionmedica/src/main/java/com/example/atencionmedica/model/Atencion.java
+++ atencionmedica/src/main/java/com/example/atencionmedica/model/Atencion.java
@@ -1,18 +1,26 @@
 package com.example.atencionmedica.model;
 
-public class Atencion {
-    private int atencionid;
-    private String atenciondoctor;
-    private String atencionresumen;
-    private String atencionlugar;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
-    public Atencion(int atencionid, String atenciondoctor, String atencionresumen, String atencionlugar){
-        this.atencionid = atencionid;
-        this.atenciondoctor = atenciondoctor;
-        this.atencionresumen = atencionresumen;
-        this.atencionlugar = atencionlugar;
-    }
-    
+@Entity
+@Table(name = "atencion")
+public class Atencion {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "atencionid")
+    private int atencionid;
+    @Column(name = "atenciondoctor")
+    private String atenciondoctor;
+    @Column(name = "atencionresumen")
+    private String atencionresumen;
+    @Column(name = "atencionlugar")
+    private String atencionlugar;
+   
     //Getters
     public int getAtencionid(){
         return atencionid;
@@ -28,5 +36,22 @@ public class Atencion {
 
     public String getAtencionlugar(){
         return atencionlugar;
+    }
+
+    //Setters
+    public void setAtencionid(Integer atencionid){
+        this.atencionid = atencionid;
+    }
+
+    public void setAtenciondoctor(String atenciondoctor){
+        this.atenciondoctor = atenciondoctor;
+    }
+
+    public void setAtencionresumen(String atencionresumen){
+        this.atencionresumen = atencionresumen;
+    }
+
+    public void setAtencionlugar(String atencionlugar){
+        this.atencionlugar = atencionlugar;
     }
 }
--- atencionmedica/src/main/java/com/example/atencionmedica/model/Paciente.java
+++ atencionmedica/src/main/java/com/example/atencionmedica/model/Paciente.java
@@ -1,28 +1,36 @@
 package com.example.atencionmedica.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
 import java.util.List;
 
+@Entity
+@Table(name = "paciente")
 public class Paciente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "pacienteid")
     private int pacienteid;
+    @Column(name = "pacienterut")   
     private int pacienterut;
+    @Column(name = "pacientenombre")
     private String pacientenombre;
+    @Column(name = "pacientecorreo")
     private String pacientecorreo;
+    @Column(name = "pacientedireccion")
     private String pacientedireccion;
+    @Column(name = "pacienteprevision")
     private String pacienteprevision;
+    @Column(name = "pacientegenero")
     private String pacientegenero;
+    @Column(name = "atenciones")
     private List<Atencion> atenciones;
     
-
-    public Paciente(int pacienteid, int pacienterut, String pacientenombre, String pacientecorreo, String pacientedireccion, String pacienteprevision, String pacientegenero, List<Atencion> atenciones){
-        this.pacienteid = pacienteid;
-        this.pacienterut = pacienterut;
-        this.pacientenombre = pacientenombre;
-        this.pacientecorreo = pacientecorreo;
-        this.pacientedireccion = pacientedireccion;
-        this.pacienteprevision = pacienteprevision;
-        this.pacientegenero = pacientegenero;
-        this.atenciones = atenciones;
-    }
 
     //GETTERS
     public int getPacienteid(){
@@ -56,4 +64,39 @@ public class Paciente {
     public List<Atencion> getAtenciones(){
         return atenciones;
     }
+
+    //SETTERS
+
+    public void setPacienteid(Integer pacienteid){
+        this.pacienteid = pacienteid;
+    }
+
+    public void setPacienterut(Integer pacienterut){
+        this.pacienterut = pacienterut;
+    }
+
+    public void setPacientenombre(String pacientenombre){
+        this.pacientenombre = pacientenombre;
+    }
+
+    public void setPacientecorreo(String pacientecorreo){
+        this.pacientecorreo = pacientecorreo;
+    }
+
+    public void setPacientedireccion(String pacientedireccion){
+        this.pacientedireccion = pacientedireccion;
+    }
+
+    public void setPacienteprevision(String pacienteprevision){
+        this.pacienteprevision = pacienteprevision;
+    }
+
+    public void setPacientegenero(String pacientegenero){
+        this.pacientegenero = pacientegenero;
+    }
+
+    public void setAtenciones(List<Atencion> atenciones){
+        this.atenciones = atenciones;
+    }
+
 }
