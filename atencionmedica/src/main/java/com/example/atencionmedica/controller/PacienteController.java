abad188160c9b43d7909f41769a0794b18012d26
agusvalenzuela <agustin.valenzuel@gmail.com>
2024-04-15 22:21:00 -0400

    Se agrega login

--- atencionmedica/src/main/java/com/example/atencionmedica/controller/PacienteController.java
+++ atencionmedica/src/main/java/com/example/atencionmedica/controller/PacienteController.java
@@ -11,6 +11,7 @@ import java.util.Optional;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,21 +19,35 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PutMapping;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 @RestController
 @RequestMapping("/pacientes") 
 public class PacienteController {
+
+    private static final Logger log = LoggerFactory.getLogger(PacienteController.class);
+
     @Autowired
     private PacienteService pacienteService;    
 
     @GetMapping()
     public List<Paciente> getPacientes(){
+        log.info("GET /pacientes");
+        log.info("Retornando todos los estudiantes");
         return pacienteService.getPacientes();
     }
     
     @GetMapping("/{pacienteid}")
-    public Optional<Paciente> getPacienteById(@PathVariable Integer pacienteid) {
-        return pacienteService.getPacienteById(pacienteid);
+    public ResponseEntity <Optional<Paciente>> getPacienteById(@PathVariable Integer pacienteid) {
+        Optional<Paciente> paciente = pacienteService.getPacienteById(pacienteid);
+        if(paciente.isEmpty()){
+            log.error("No se encontró el estudiante con ID {}", pacienteid);
+            return ResponseEntity.notFound().build();
+        }
+        log.info("Estudiante encontrado con éxito");
+        return ResponseEntity.ok(pacienteService.getPacienteById(pacienteid));
     }
 
     @PostMapping
